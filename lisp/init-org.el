3249665e07efd87d439fb32e52fec1fcfa3d920b
qinrongjun <qinrongjun@outlook.com>
2023-09-08 10:35:26 +0800

    feat(module): plantuml

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,9 +2,13 @@
   :hook
   ((org-mode . org-indent-mode))
   :config
+  (setq org-plantuml-jar-path "~/.emacs.d/plantuml.jar")
   (org-babel-do-load-languages
    'org-babel-load-languages
-   '((C . t)))
+   '((C . t)
+     (java . t)
+     (plantuml . t)))
+  
   :bind
   (("C-c o a" . 'org-agenda)))
 
